282989142707beab0d4e03007c1e6afc882a8533
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-03 16:18:24 +0200

    apparition du serpent

--- snake.py
+++ snake.py
@@ -7,8 +7,16 @@ WIDTH = 600
 HEIGHT = 600
 CASE_DIM = 20
 IPS = 1
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+
+snake_pos = [
+    (10, 15),
+    (11, 15),
+    (12, 15),
+]
 run = True
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -24,6 +32,13 @@ def set_background(white, black, dim):
                 pg.draw.rect(screen, black, rect)
 
 set_background(WHITE, BLACK, CASE_DIM)
+
+def snake(screen, snake_pos, dim, color):
+    for i in range(len(snake_pos)):
+        snake = pg.Rect(snake_pos[i][0]*dim, snake_pos[i][1]*dim, dim, dim)
+        pg.draw.rect(screen, color, snake)
+
+snake(screen, snake_pos, CASE_DIM, GREEN)
 
 while run:
     clock.tick(IPS)
