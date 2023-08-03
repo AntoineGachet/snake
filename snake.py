e4d513707a441ffeb6dab4e81e8967eeaebf3d32
AntoineGachet <antoine.gachet05@gmail.com>
2023-08-03 17:53:30 +0200

    functional directions for snake

--- snake.py
+++ snake.py
@@ -6,7 +6,7 @@ pg.init()
 WIDTH = 600
 HEIGHT = 600
 CASE_DIM = 20
-IPS = 1
+IPS = 5
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -16,7 +16,9 @@ snake_pos = [
     (10, 15),
     (11, 15),
     (12, 15),
+    (13, 15),
 ]
+dir = (-1, 0)
 run = True
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -33,14 +35,25 @@ def set_background(white, black, dim):
 
 set_background(WHITE, BLACK, CASE_DIM)
 
-def snake(screen, snake_pos, dim, color):
-    for i in range(len(snake_pos)):
+def init_snake(screen, snake_pos, dim, color):
+    for i in range(len(snake_pos)-1):
         snake = pg.Rect(snake_pos[i][0]*dim, snake_pos[i][1]*dim, dim, dim)
         pg.draw.rect(screen, color, snake)
 
-snake(screen, snake_pos, CASE_DIM, GREEN)
+def update_snake(screen, snake_pos, dim, color, dir):
+    snake_pos.insert(0, tuple(map(lambda i, j: i+j, dir, snake_pos[0])))
+    snake_pos.pop(-1)
+    return snake_pos
+
+def init_case(screen, snake_pos, dim=CASE_DIM, white=WHITE, black=BLACK):
+    switched_case = snake_pos[-1]
+    rect = pg.Rect(switched_case[0]*dim, switched_case[1]*dim, dim, dim)
+    color = white if (snake_pos[-1][0]+snake_pos[-1][1])%2==0 else black
+    pg.draw.rect(screen, color, rect)
+
 
 while run:
+
     clock.tick(IPS)
 
     for event in pg.event.get():
@@ -51,8 +64,23 @@ while run:
             if event.key == pg.K_q:
                 run = False
 
-    # random_color = (randint(0,255), randint(0,255), randint(0,255))
-    # screen.fill(random_color)
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP:
+                dir = (0, -1)
+            elif event.key == pg.K_DOWN:
+                dir = (0, 1)
+
+            elif event.key == pg.K_LEFT:
+                dir = (-1, 0)
+            elif event.key == pg.K_RIGHT:
+                dir = (1, 0)
+
+    if dir != (0, 0):
+        init_case(screen, snake_pos)
+    update_snake(screen, snake_pos, CASE_DIM, GREEN, dir)
+    init_snake(screen, snake_pos, CASE_DIM, GREEN)
+
+
     pg.display.update()
 
 pg.quit()
